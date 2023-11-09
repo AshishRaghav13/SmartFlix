cccc619244d93d03e2e64b630684924ccb414cf4
Ashish Raghav <ashishraghav2468@gmail.com>
2023-11-10 01:27:29 +0530

    TMDB API data fetch && Built Main Container

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { addUser,removeUser } from "../utils/userSlice";
+import { LOGO, USER_AVATAR } from "../utils/constants";
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -18,7 +19,7 @@ const Header = () => {
     
   }
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         const {uid,email,displayName,photoURL} = user;
         dispatch(addUser({uid:uid,email:email,displayName:displayName,photoURL:photoURL}));
@@ -28,19 +29,21 @@ const Header = () => {
         navigate("/")
       }
     });
+    // unscribe when component unmounts
+    return ()=> unsubscribe();
   }, []);
 
   return (
     <div className="absolute px-14 py-2 w-screen bg-gradient-to-b from-black z-10 flex justify-between">
       <img
         className="w-48"
-        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        src={LOGO}
         alt="logo"
       />
       <div className="flex p-4">
         <img className="w-12 h-12"
           alt="userIcon"
-          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          src={USER_AVATAR}
         />
         <button onClick={handleSignOut} className="p-2 font-bold text-white">Sign Out</button>
       </div>
